8f26216106426103bdfcecd1f8e9b0370d739d75
AchyutPL <84321408+AchyutPL@users.noreply.github.com>
2021-08-15 07:34:24 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -267,12 +267,7 @@ function App() {
     "#B33300",
     "#CC80CC",
   ];
-  const data = [
-    { name: "Facebook", users: 2000000000 },
-    { name: "Instagram", users: 1500000000 },
-    { name: "Twiter", users: 1000000000 },
-    { name: "Telegram", users: 500000000 },
-  ];
+
   return (
     <>
       <div className="App">
